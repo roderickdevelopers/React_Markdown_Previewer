3effa0a334d56f8285612d0ebb6365aeb342f38b
Matthew <roderickdevelopers@gmail.com>
2020-09-03 16:31:59 -0400

    Styling_done

--- src/components/structural/textArea/TextArea.jsx
+++ src/components/structural/textArea/TextArea.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
-// import styles from '/components/structural/textArea/TextArea.module.scss';
+import styles from 'components/structural/textArea/TextArea.module.scss';
 import { PropTypes } from 'prop-types';
 
 function TextArea(props) {
   return (
-    <div className="text_area_container">
-      <textarea 
-        id="editor"
-        placeholder="Type Markdown Here!"
+    <div className={styles.text_area_container}>
+      <label className={styles.textarea} htmlFor="textarea">Enter Markdown here:</label>
+      <textarea
+        id="textarea"
+        className={styles.editor}
         value={props.markdown}
         onChange={(e) => {
-           this.props.updateMarkdown(e.target.value);}}>
+          props.updateMarkdown(e.target.value);}}>
         {props.markdown}
-       </textarea>
-     </div>
+      </textarea>
+    </div>
   );
 }
 
-TextArea.propTypes = {
+TextArea.propTypes = { 
   markdown: PropTypes.string,
   updateMarkdown: PropTypes.func
 }
-
 
 export default TextArea;
